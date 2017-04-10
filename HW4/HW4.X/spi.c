9d3f2b0f4da874203811629fa6faa3425580dcf4
Jerry Yang <sovermind11@gmail.com>
2017-04-10 16:44:40 -0500

    in progress

--- HW4/HW4.X/spi.c
+++ HW4/HW4.X/spi.c
@@ -1,7 +1,6 @@
 #include "spi.h"                   
 #include <xc.h>
 
-#define CS LATBbits.LATB15
 void initSPI1() {
     //make pin 17, which is RPB8, as SDO1
     RPB8Rbits.RPB8R = 0b0011;
@@ -13,12 +12,21 @@ void initSPI1() {
     //sck is B14
     
     //set up SPI1
-    SPI1CON = 0;     //trun off the spi module and reset it
+    SPI1CON = 0;     //turn off the spi module and reset it
     SPI1BUF;         //clear rx buffer by reading from it
     SPI1BRG = 0x1;      //SPI4BRG = (48000000/(2*desired))-1, fastest rate = 12MHz
+    SPI1STATbits.SPIROV = 0;    //clear overflow bit
+    SPI1CONbits.CKE = 1;        //data changes when clock goes from hi to low
+    SPI1CONbits.CKP = 0;
+    SPI1CONbits.MSTEN = 1;      //master operation
+    SPI1CONbits.ON = 1;
     
 }
 
 char SPI1_IO(char write) {
-    
+    SPI1BUF = write;
+    while(!SPI1STATbits.SPIRBF) {
+        ;
+    }
+    return SPI1BUF;
 }
--- HW4/HW4.X/spi.h
+++ HW4/HW4.X/spi.h
@@ -1,6 +1,8 @@
 #ifndef SPI__H__
 #define SPI__H__
 
+#define CS LATBbits.LATB15
+
 void initSPI1();
 char SPI1_IO(char write);
 
