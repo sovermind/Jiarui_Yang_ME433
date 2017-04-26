7cf7ce278cc7c3413832c99fad2ed2cea0139721
Jerry Yang <sovermind11@gmail.com>
2017-04-25 22:53:24 -0500

    add previous files

--- HW9/HW9/firmware/src/app.c
+++ HW9/HW9/firmware/src/app.c
@@ -51,13 +51,15 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 #include "app.h"
 #include <stdio.h>
 #include <xc.h>
+#include "ILI9163C.h"
+#include "i2c_master_noint.h"
 
 // *****************************************************************************
 // *****************************************************************************
 // Section: Global Data Definitions
 // *****************************************************************************
 // *****************************************************************************
-
+#define IMU_ADDR 0b1101011  //default address for IMU board 0b1101011
 uint8_t APP_MAKE_BUFFER_DMA_READY dataOut[APP_READ_BUFFER_SIZE];
 uint8_t APP_MAKE_BUFFER_DMA_READY readBuffer[APP_READ_BUFFER_SIZE];
 int len, i = 0;
@@ -77,14 +79,188 @@ int startTime = 0;
 
 APP_DATA appData;
 
+float temp = 0; 
+float g_x = 0;       //35mdps/LSB from data sheet page 15
+float g_y = 0;
+float g_z = 0;
+float a_x = 0;      //0.061mg/LSB from data sheet page 15
+float a_y = 0;
+float a_z = 0;
+
+float roll = 0;
+float pitch = 0;
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Application Callback Functions
 // *****************************************************************************
 // *****************************************************************************
 
-/* TODO:  Add any necessary callback functions.
- */
+void writetoIMU(unsigned char register_addr, unsigned char send_value);
+
+//all functions to display on screen, copied from HW 6
+//draw the char c on x,y location with color
+void display_character(unsigned char c, unsigned short x, unsigned short y, unsigned short char_color, unsigned short back_color) {
+    char ascii_row = c - 0x20;
+    int i=0;
+    int j=0;
+    
+    for (i=0;i<5;i++) {
+        if ((x+i) < 128) {
+            for (j=0;j<8;j++) {
+                if ((ASCII[ascii_row][i]>>j)&1 == 1) {
+                    if ((y+j)<128) {
+                        LCD_drawPixel(x+i,y+j,char_color);    //write the charactor color
+                    }
+                }
+                else {
+                    if ((y+j)<128) {
+                        LCD_drawPixel(x+i,y+j,back_color);    //clear the background color
+                    }
+                }
+            }
+        }
+    }
+}
+
+void display_String(unsigned char* c,unsigned short x, unsigned short y, unsigned short char_color, unsigned short back_color){
+    char cc = *c;
+    unsigned short count =0;
+    while (cc != 0) {
+        display_character(cc,x+count*5,y,char_color,back_color);
+        count ++;
+        cc = *(c+count);
+    }
+}
+
+//draw the progress bar at x,y position with bar_color and can define the thickness
+//It will clear all other pixels within (x+thickness) locations
+//thickness and width are in pixels
+void draw_h_bar(unsigned short thickness,unsigned short width, unsigned short x, unsigned short y, unsigned short bar_color, unsigned short back_color) {
+    int w = 0;
+    int t = 0;
+    //check if the bar will exceed the boundry
+    if (x+width > 128) {
+        width = 127 - x;
+    }
+    if (y+thickness > 128) {
+        thickness = 127 - y;
+    }
+    
+    for (w = 0;w<128;w++) {
+        for (t = y;t<y+thickness;t++) {
+            if (w<x || w>(x+width)) {
+                LCD_drawPixel(w,t,back_color);
+            }
+            else {
+                LCD_drawPixel(w,t,bar_color);
+            }
+        }
+    }
+}
+
+//draw the verticle bar, will clear a region of bar width and specified y region
+void draw_v_bar(unsigned short y_up_lim,unsigned short y_low_lim, unsigned short thickness,unsigned short width, unsigned short x, unsigned short y, unsigned short bar_color, unsigned short back_color) {
+    int w = 0;
+    int t = 0;
+    
+    for (w = x;w<x+width;w++) {
+        for (t = y_up_lim;t<y_low_lim;t++) {
+            if (t>=y && t<=y+thickness) {
+                LCD_drawPixel(w,t,bar_color);
+            }
+            else {
+                LCD_drawPixel(w,t,back_color);
+            }
+        }
+    }
+}
+
+void initIMU() {
+    //turn off the analog
+    ANSELBbits.ANSB2 = 0;
+    ANSELBbits.ANSB3 = 0;
+    //i2c init
+    i2c_master_setup();
+    //set up accl and gyro
+    writetoIMU(0x10,0b10000010);  //CTRL1_XL accelerator
+    writetoIMU(0x11,0b10001000);  //CTRL2_G gyro
+    writetoIMU(0x12,0b00000100);  //CTRL3_C control
+    
+}
+
+unsigned char readIMU(unsigned char register_addr) {
+    i2c_master_start();
+    i2c_master_send(IMU_ADDR<<1|0);//send address, write mode
+    i2c_master_send(register_addr);              //write to register
+    i2c_master_restart();
+    i2c_master_send(IMU_ADDR<<1|1);//send address, read mode
+    unsigned char r = i2c_master_recv();//save the value
+    i2c_master_ack(1);                  //tell slave that master get the value
+    i2c_master_stop();
+    
+    return r;
+
+}
+
+void IMU_read_multiple(unsigned char register_addr, unsigned char * data, int length) {
+    i2c_master_start();
+    i2c_master_send(IMU_ADDR<<1|0);//send address, write mode
+    i2c_master_send(register_addr);              //write to register
+    i2c_master_restart();
+    i2c_master_send(IMU_ADDR<<1|1);//send address, read mode
+    int count = 0;
+    while (1) {
+        data[count] = i2c_master_recv();//save the value
+        if (count == (length-1)) {
+            i2c_master_ack(1);                  //tell slave no more byte should be send
+            break;
+        } 
+        else {
+            i2c_master_ack(0);                  //tell slave to send another byte
+        }
+        count ++;
+    }
+    i2c_master_stop();
+}
+
+void writetoIMU(unsigned char register_addr, unsigned char send_value) {
+    i2c_master_start();
+    i2c_master_send(IMU_ADDR<<1|0);//send address, write mode
+    i2c_master_send(register_addr);              //write to register
+    i2c_master_send(send_value);        //sent in value
+    i2c_master_stop();
+}
+
+void read_all_value() {
+    int data_length = 14;   //which means all data
+    unsigned char alldata[data_length];
+    IMU_read_multiple(0x20,alldata,data_length);
+    //seperate all data
+    unsigned char L_bits[data_length/2];
+    unsigned char H_bits[data_length/2];
+    int i=0;
+    for (i = 0;i<data_length/2;i++) {
+        L_bits[i] = alldata[i*2];
+        H_bits[i] = alldata[i*2+1];
+    }
+    signed short temp_raw = (H_bits[0])<<8|(L_bits[0]);
+    signed short g_x_raw = (H_bits[1])<<8|(L_bits[1]);
+    signed short g_y_raw = (H_bits[2])<<8|(L_bits[2]);
+    signed short g_z_raw = (H_bits[3])<<8|(L_bits[3]);
+    signed short a_x_raw = (H_bits[4])<<8|(L_bits[4]);
+    signed short a_y_raw = (H_bits[5])<<8|(L_bits[5]);
+    signed short a_z_raw = (H_bits[6])<<8|(L_bits[6]);
+        
+    float temp = temp_raw*0.0625; 
+    g_x = g_x_raw*35.0/1000.0;       //35mdps/LSB from data sheet page 15
+    g_y = g_y_raw*35.0/1000.0;
+    g_z = g_z_raw*35.0/1000.0;
+    a_x = a_x_raw*0.061/1000.0;      //0.061mg/LSB from data sheet page 15
+    a_y = a_y_raw*0.061/1000.0;
+    a_z = a_z_raw*0.061/1000.0;
+    a_z = -a_z;
+}
 
 /*******************************************************
  * USB CDC Device Events - Application Event Handler
@@ -299,6 +475,51 @@ bool APP_StateReset(void) {
 void APP_Initialize(void) {
     /* Place the App state machine in its initial state. */
     appData.state = APP_STATE_INIT;
+    
+    //LCD and IMU initialization
+    __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+    TRISBbits.TRISB4 = 1;
+    TRISAbits.TRISA4 = 0;
+    LATAbits.LATA4 = 1;
+    //initialize IMU
+    initIMU();
+    //init SPI1 and LCD
+    SPI1_init();
+    LCD_init();
+    
+    __builtin_enable_interrupts();
+    
+    //clear the screen
+    LCD_clearScreen(WHITE);
+//    display_String("Hello, world",28,32,BLACK,WHITE);
+    
+    //read from who am I
+    unsigned char who_am_i_addr = 0x0F;
+    unsigned char who_am_i_value = readIMU(who_am_i_addr);
+    if (who_am_i_value == 0b01101001){
+        display_String("Connected!",15,10,BLACK,WHITE);
+    }
+
+//    //test to see if i can read from accel
+//    unsigned char test1 = readIMU(0x28);
+//    unsigned char test2 = readIMU(0X29);
+//    unsigned char str[100];
+//    sprintf(str,"L = %x, H = %x",test1,test2);
+//    display_String(str,15,25,BLACK,WHITE);
 
     /* Device Layer Handle  */
     appData.deviceHandle = USB_DEVICE_HANDLE_INVALID;
